7263dff3d7a5c40c4fb1dcf69a9d19f6e16dae4d
Aline <line.asramos@gmail.com>
2021-04-03 21:52:46 -0300

    Example UI

---
+++ src/main/scala/sudoku/ui/example/CelsiusConverter2.scala
@@ -0,0 +1,41 @@
+package sudoku.ui.example
+
+import scala.swing._
+import scala.swing.event._
+
+object CelsiusConverter2 extends SimpleSwingApplication {
+  def newField: TextField = new TextField {
+    text = "0"
+    columns = 5
+    horizontalAlignment = Alignment.Right
+  }
+
+  val celsius = newField
+  val fahrenheit = newField
+
+  listenTo(fahrenheit, celsius)
+  reactions += {
+    case EditDone(`fahrenheit`) =>
+      val f = Integer.parseInt(fahrenheit.text)
+      val c = (f - 32) * 5 / 9
+      celsius.text = c.toString
+    case EditDone(`celsius`) =>
+      val c = Integer.parseInt(celsius.text)
+      val f = c * 9 / 5 + 32
+      fahrenheit.text = f.toString
+  }
+
+  lazy val ui = new FlowPanel(
+    celsius,
+    new Label(" Celsius  =  "),
+    fahrenheit,
+    new Label(" Fahrenheit")
+  ) {
+    border = Swing.EmptyBorder(15, 10, 10, 10)
+  }
+
+  def top: Frame = new MainFrame {
+    title = "Convert Celsius / Fahrenheit"
+    contents = ui
+  }
+}
